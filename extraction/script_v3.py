61e12aeea9e1479381b6f1eee9f876ed01ac262e
VanGaigher <vanessa.mat.sedu@gmail.com>
2025-01-05 20:05:00 +0000

    inserindo projeto atualizado

---
+++ extraction/script_v3.py
@@ -0,0 +1,102 @@
+import requests
+import pandas as pd
+from sqlalchemy import create_engine
+from snowflake.sqlalchemy import URL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class JobicyAPI:
+
+    #Definindo os atributos da classe
+    def __init__(self, base_url:str, industry: str, count: int):
+        self.base_url = base_url
+        self.industry =industry
+        self.count = count
+        self.data = None
+    
+    #método para requisitar os dados da API e salvar num atribuito da própria classe
+    def fetch_data(self):
+        url = f"{self.base_url}?count={self.count}&industry={self.industry}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            self.data = response.json()
+        else:
+            response.raise_for_status()
+    
+    # método para carregar dados do self.data em um dataframe
+    def get_jobs_data(self):
+        if self.data and 'jobs' in self.data:
+            return pd.DataFrame(self.data['jobs'])
+        else:
+            return pd.DataFrame()
+
+# classe para conectar e salvar dados no Snowflake
+class Snowflake:
+    def __init__(self, account: str, user:str, password:str, database:str, schema:str, warehouse:str):
+        self.engine = create_engine(URL(
+            account = account,
+            user= user,
+            password= password,
+            database= database,
+            schema= schema,
+            warehouse= warehouse,
+
+        ))
+    
+    #salvar dados no snowflake a partir de um dataframe
+    def save_to_snowflake(self, df: pd.DataFrame, table_name: str):
+        df.to_sql(table_name,self.engine, if_exists= 'replace', index=False)
+
+def main():
+    api = JobicyAPI(
+        base_url= "https://jobicy.com/api/v2/remote-jobs",
+        industry= "data-science",
+        count= 10
+    )
+    api.fetch_data()
+
+    jobs_df = api.get_jobs_data()
+
+    # verificando se o dataframe não est+a vazio
+    if not jobs_df.empty:
+
+        #Ajustar nome das colunas
+        jobs_df.columns = [
+            "ID", "URL", "jobSlug", "jobTitle", "companyName", "companyLogo", "jobIndustry", 
+            "jobType", "jobGeo", "jobLevel", "jobExcerpt", "jobDescription", "pubDate", 
+            "annualSalaryMin", "annualSalaryMax", "salaryCurrency" 
+            ]
+        
+        #Alinhando os tipos de dados
+        jobs_df["ID"]= jobs_df["ID"].astype(int)
+        string_columns = [
+            "URL", "jobSlug", "jobTitle", "companyName", "companyLogo", "jobIndustry", 
+            "jobType", "jobGeo", "jobLevel", "jobExcerpt", "jobDescription", "pubDate", 
+            "annualSalaryMin", "annualSalaryMax", "salaryCurrency" 
+            ]
+        for column in string_columns:
+            jobs_df[column] = jobs_df[column].astype(str)
+
+        # Tratar os valor nulos
+        jobs_df = jobs_df.fillna('')
+
+        saver= Snowflake(
+            account = os.getenv("ACCOUNT"),
+            user= os.getenv("USER"),
+            password= os.getenv("PASSWORD"),
+            database= os.getenv("DATABASE"),
+            schema= os.getenv("SCHEMA"),
+            warehouse= os.getenv("WAREHOUSE"),
+        )
+
+        saver.save_to_snowflake(jobs_df, table_name="listagem_trabalhos_remotos")
+        print("Dados salvos com sucesso no Snowflake!")
+    
+    else:
+        print("Não existem dados a serem salvos no Snowflake")
+
+
+if __name__ == '__main__':
+    main()
